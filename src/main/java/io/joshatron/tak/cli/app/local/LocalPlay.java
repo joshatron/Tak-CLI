8f1b9d62e06bb94dac02559da63a5ec2ab39e8bd
Joshua Leger <legermjoshua@gmail.com>
2019-01-27 10:06:05 -0800

    Updated AI to latest version

--- src/main/java/io/joshatron/tak/cli/app/local/LocalPlay.java
+++ src/main/java/io/joshatron/tak/cli/app/local/LocalPlay.java
@@ -1,7 +1,7 @@
 package io.joshatron.tak.cli.app.local;
 
-import io.joshatron.tak.ai.player.DefensiveEvaluator;
-import io.joshatron.tak.ai.player.MiniMaxPlayer;
+import io.joshatron.tak.ai.player.AI;
+import io.joshatron.tak.ai.player.AIFactory;
 import io.joshatron.tak.engine.exception.TakEngineException;
 import io.joshatron.tak.engine.game.GameSetResult;
 import io.joshatron.tak.engine.game.Games;
@@ -12,7 +12,8 @@ public class LocalPlay {
 
     public static void play(int games, int size, Player firstPlayer, String whiteType, String blackType) {
         try {
-            GameSetResult results = new Games(games, size, firstPlayer, createPlayer(whiteType, size), createPlayer(blackType, size), new CLIHooks()).playGames();
+            GameSetResult results = new Games(games, size, firstPlayer, createPlayer(whiteType, size, Player.WHITE, firstPlayer),
+                    createPlayer(blackType, size, Player.BLACK, firstPlayer), new CLIHooks()).playGames();
 
             if (results.getWhiteWins() > results.getBlackWins()) {
                 System.out.println("White is the winner " + results.getWhiteWins() + ":" + results.getBlackWins());
@@ -27,31 +28,14 @@ public class LocalPlay {
         }
     }
 
-    private static TakPlayer createPlayer(String type, int size) {
+    private static TakPlayer createPlayer(String type, int size, Player player, Player first) {
         if(type.equalsIgnoreCase("human")) {
             return new HumanPlayer();
         }
         else if(type.equalsIgnoreCase("ai")) {
-            return new MiniMaxPlayer(new DefensiveEvaluator(), depthFromBoardSize(size));
+            return AIFactory.createPlayer(AI.DEFENSIVE_MINIMAX, size, player, first);
         }
 
         return null;
-    }
-
-    private static int depthFromBoardSize(int size) {
-        switch (size) {
-            case 3:
-                return 5;
-            case 4:
-                return 4;
-            case 5:
-                return 3;
-            case 6:
-                return 3;
-            case 8:
-                return 2;
-            default:
-                return 2;
-        }
     }
 }
