512a0b7f48ffb57539ddd70faeab2032a0a054b9
Joshua Leger <legermjoshua@gmail.com>
2019-02-05 22:18:03 -0800

    Started working on turn completer

---
+++ src/main/java/io/joshatron/tak/cli/app/commands/TurnCompleter.java
@@ -0,0 +1,35 @@
+package io.joshatron.tak.cli.app.commands;
+
+import io.joshatron.tak.engine.turn.Turn;
+import org.jline.reader.Candidate;
+import org.jline.reader.Completer;
+import org.jline.reader.LineReader;
+import org.jline.reader.ParsedLine;
+import org.jline.utils.AttributedString;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class TurnCompleter implements Completer {
+
+    private List<String> turns;
+
+    public TurnCompleter(List<Turn> turns) {
+        this.turns = turns.stream().map(Turn::toString).collect(Collectors.toList());
+    }
+
+    @Override
+    public void complete(LineReader lineReader, ParsedLine parsedLine, List<Candidate> list) {
+        if(parsedLine != null && list != null) {
+            ArrayList<Candidate> candidates = new ArrayList<>();
+
+            List<String> possible = turns.stream().filter(element -> element.startsWith(parsedLine.line())).collect(Collectors.toList());
+            for(String turn : possible) {
+                candidates.add(new Candidate(AttributedString.stripAnsi(turn), turn, null, null, null, null, true));
+            }
+
+            list.addAll(candidates);
+        }
+    }
+}
