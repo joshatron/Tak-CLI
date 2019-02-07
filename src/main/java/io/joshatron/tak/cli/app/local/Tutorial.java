5153a08a609104724e83723b9e1fd2cdadc220d9
Joshua Leger <legermjoshua@gmail.com>
2019-02-06 23:23:29 -0800

    Added pdf rules and state to tutorial

--- src/main/java/io/joshatron/tak/cli/app/local/Tutorial.java
+++ src/main/java/io/joshatron/tak/cli/app/local/Tutorial.java
@@ -1,8 +1,19 @@
 package io.joshatron.tak.cli.app.local;
 
+import io.joshatron.tak.engine.exception.TakEngineException;
+import io.joshatron.tak.engine.game.GameState;
+import io.joshatron.tak.engine.game.Player;
+
 public class Tutorial {
 
     public static void play() {
-        System.out.println("Welcome to the Tak tutorial. Here you will learn the basics for how to play the game of Tak.");
+        try {
+            GameState testState = new GameState(Player.WHITE, 5);
+
+            System.out.println("Welcome to the Tak tutorial. Here you will learn the basics for how to play the game of Tak.");
+        }
+        catch(TakEngineException e) {
+            System.out.println("Something went wrong with the engine.");
+        }
     }
 }
